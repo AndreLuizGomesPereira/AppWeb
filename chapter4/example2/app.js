8fc219f59dba18ac813c3f6dea44af31e69908f0
André Luiz <andregomesenator@gmail.com>
2020-03-09 22:00:01 -0300

    Exercicio completo com iteratividade JQuery.

--- chapter4/example2/app.js
+++ chapter4/example2/app.js
@@ -3,7 +3,18 @@ var main = function () {
     $(".comment-input button").on("click", function (event) {
         var $novoComentario;
 
+        if ($(".comment-input input").val() !== "") {
+            $novoComentario = $("<p>").text($(".comment-input input").val());
+            $novoComentario.hide();
+            $(".comments").append($novoComentario);
+            $novoComentario.fadeIn();
+            $(".comment-input input").val("");
+        }
+    });
+    $(".comment-input input").on("keypress", function () {
+        var $novoComentario;
 
+        if (event.keyCode === 13) {
             if ($(".comment-input input").val() !== "") {
                 $novoComentario = $("<p>").text($(".comment-input input").val());
                 $novoComentario.hide();
@@ -11,6 +22,8 @@ var main = function () {
                 $novoComentario.fadeIn();
                 $(".comment-input input").val("");
             }
-    });
+        }
+    })
+
 };
 $(document).ready(main);
