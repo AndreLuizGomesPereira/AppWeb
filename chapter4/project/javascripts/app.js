d51ae427dd44a86a943eb5419e42febd774257cc
André Luiz <andregomesenator@gmail.com>
2020-03-09 23:27:26 -0300

    Html como exemplo da função adicionando script 'fadeOut' em <footer>.

--- chapter4/project/javascripts/app.js
+++ chapter4/project/javascripts/app.js
@@ -7,10 +7,13 @@ var main = function(){
     var $content = $("<h2>Olá como vai você?</h2>").hide();
     var $moreContent = $("<h3>Eu estou bem!</h3>").hide();
 
-    $("body").append($content);
+    $("body main").append($content);
     $content.slideDown(2000, function(){
-        $("body").append($moreContent);
+        $("body main").append($moreContent);
         $moreContent.fadeIn();
+        $("footer p").fadeOut(1000, function(){
+            $("footer p").remove();
+        })
     });
 
 
