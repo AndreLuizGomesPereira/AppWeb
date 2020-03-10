f4ee3076ec352aea213ffad54c75bac3eef32348
André Luiz <andregomesenator@gmail.com>
2020-03-09 21:01:16 -0300

    Alteração do script para outra forma de comentar na linha estilizada.

--- chapter4/example2/app.js
+++ chapter4/example2/app.js
@@ -1,7 +1,9 @@
 var main = function () {
     "use strict";
-    $(".comment-input button").on("click", function (even) {
-        $(".comments").append("<p>This is new a comment!</p>")
+    $(".comment-input button").on("click", function (event) {
+        var $new_comment = $("<p>");
+        $new_comment.text("this is a new comment");
+        $('.comments').append($new_comment);
     });
 };
 $(document).ready(main);
