47fe8553a351cec7250ada90910731f25b79978b
André Luiz <andregomesenator@gmail.com>
2020-03-09 21:14:05 -0300

    Script rodando para inserir comentários

--- chapter4/example2/app.js
+++ chapter4/example2/app.js
@@ -1,9 +1,8 @@
 var main = function () {
     "use strict";
     $(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>");
-        $new_comment.text("this is a new comment");
-        $('.comments').append($new_comment);
+        var $novoComentario = $("<p>").text( $(".comment-input input").val());
+        $(".comments").append($novoComentario);
     });
 };
 $(document).ready(main);
