09162dff172c426059d22d69f56fd4772515f2ea
André Luiz <andregomesenator@gmail.com>
2020-03-09 22:16:12 -0300

    Finalização com o encurtamento do código para manutenção posterior.

--- chapter4/example2/app.js
+++ chapter4/example2/app.js
@@ -1,29 +1,23 @@
 var main = function () {
     "use strict";
-    $(".comment-input button").on("click", function (event) {
-        var $novoComentario;
+    var addCommentFromInputBox = function () {
+        var $newComment;
 
         if ($(".comment-input input").val() !== "") {
-            $novoComentario = $("<p>").text($(".comment-input input").val());
-            $novoComentario.hide();
-            $(".comments").append($novoComentario);
-            $novoComentario.fadeIn();
+            $newComment = $("<p>").text($(".comment-input input").val());
+            $newComment.hide();
+            $(".comments").append($newComment);
+            $newComment.fadeIn();
             $(".comment-input input").val("");
         }
+    };
+    $(".comment-input button").on("click", function () {
+        addCommentFromInputBox();
     });
-    $(".comment-input input").on("keypress", function () {
-        var $novoComentario;
-
+    $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode === 13) {
-            if ($(".comment-input input").val() !== "") {
-                $novoComentario = $("<p>").text($(".comment-input input").val());
-                $novoComentario.hide();
-                $(".comments").append($novoComentario);
-                $novoComentario.fadeIn();
-                $(".comment-input input").val("");
-            }
+            addCommentFromInputBox();
         }
-    })
-
+    });
 };
 $(document).ready(main);
