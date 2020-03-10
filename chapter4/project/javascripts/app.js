34d8d074de8f8fec27112f99bfba58de9d4e801d
André Luiz <andregomesenator@gmail.com>
2020-03-09 23:12:48 -0300

    Utilizado dois recuros, 'SlideDown e FadeIn'.

--- chapter4/project/javascripts/app.js
+++ chapter4/project/javascripts/app.js
@@ -1,6 +1,16 @@
-var main = function(){
+/*var main = function () {
     var $content = $("<div>Hello World!</div>").hide();
     $("body").append($content);
     $content.slideDown(2000);
+}*/
+var main = function(){
+    var $content = $("<h2>Olá como vai você?</h2>").hide();
+    var $moreContent = $("<h3>Eu estou bem!</h3>").hide();
+
+    $("body").append($content);
+    $content.slideDown(2000);
+
+    $("body").append($moreContent);
+    $moreContent.fadeIn(2000);
 }
 $(document).ready(main);
