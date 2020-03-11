6c9dcc8db67297f2bee5223e410bf66616f72bca
André Luiz <andregomesenator@gmail.com>
2020-03-11 19:13:10 -0300

    Refatorando o código usando um laço 'for'.

--- chapter4/project/javascripts/app.js
+++ chapter4/project/javascripts/app.js
@@ -1,23 +1,13 @@
 var main = function(){
     "Uso restrito";
-    var makeTabActive = function(tabNumber){
-        //constroi o seletor a partir de tabNumber
-        var abaSelecionada = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(".tabs span").removeClass("active");
-        $(abaSelecionada).addClass("active");
-        $("main .content").empty();
-    };
-    $(".tabs a:nth-child(1)").on("click", function(){
-        makeTabActive(1);
-        return false;
-    });
-    $(".tabs a:nth-child(2)").on("click", function(){
-        makeTabActive(2);
-        return false;
-    });
-    $(".tabs a:nth-child(3)").on("click", function(){
-        makeTabActive(3);
-        return false;
-    });
+    var tabNumber;
+    for(tabNumber = 1; tabNumber <=3; tabNumber++){
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ")span";
+        $(tabSelector).on("click", function (event) {
+            $(".tabs span").removeClass("active");
+            $(event.target).addClass("active");
+            return false;
+        });
+    }
 };
 $(document).ready(main);
