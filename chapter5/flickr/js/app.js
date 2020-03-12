211880f3f03e55c9ba7f742bad647bf75f6ad7a2
André Luiz <andregomesenator@gmail.com>
2020-03-12 17:48:57 -0300

    Script para consumir JSON da api Flickr.

--- chapter5/flickr/js/app.js
+++ chapter5/flickr/js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+var main = function (){
+
+    //esta, na realidade, é somente uma string, porem eu a dividi em duas linhas para torna-la mais legivel
+    var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=dogs&format=json&jsoncallback=?";
+
+    $.getJSON(url, function(flickrResponse){
+        //iremos simpesmente exibir a resposta no console por enquanto
+        console.log(flickrResponse);
+    });
+};
+$(document).ready(main);
