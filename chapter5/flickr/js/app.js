5f5aa8ffc056703646aa950c8af4bafe90b82a02
André Luiz <andregomesenator@gmail.com>
2020-03-13 17:22:18 -0300

    Atualização do script para visualizar as fotos com a tag img com classe photos.

--- chapter5/flickr/js/app.js
+++ chapter5/flickr/js/app.js
@@ -3,17 +3,18 @@ var main = function (){
     //esta, na realidade é somente uma string, porém eu a dividi em duas linhas para torna-la mais legivel
     var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=dogs&format=json&jsoncallback=?";
 
-    $.getJSON(url, function(flickrResponse){
-        flickrResponse.items.forEach(function (photo){
-            //cria um novo elemento jQuery oara armazenar a imagem
-            var $img = $("<img>");
-            
-            //define o atributo usando url contido na resposta
-            $img.attr("src", photo.media.m);
+   $.getJSON(url, function(flickrResponse){
+       flickrResponse.items.forEach(function(photo){
+           //cria um novo elemento jQuery para armazenar a imagem, mas oculta-o para que possamos fazer o fadeIn
+           var $img = $("<img>").hide();
 
-            //associa a tag img ao elemento main photos
-            $("main.photos").append($img);
-        });
-    });
+           //define o atributo usando url contido na resposta
+           $img.attr("src", photo.media.m);
+
+           //associa a tag img ao elemento main photos e faz o seu fadeIn
+           $("main .photos").append($img);
+           $img.fadeIn();
+       });
+   });
 };
 $(document).ready(main);
