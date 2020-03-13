76d5eadf4f4a7887482d66bc1a05d1664224b0a8
André Luiz <andregomesenator@gmail.com>
2020-03-13 16:06:10 -0300

    Visualização do arquivo JSON no console.

--- chapter5/flickr/js/app.js
+++ chapter5/flickr/js/app.js
@@ -1,10 +1,10 @@
 var main = function (){
 
-    //esta, na realidade, é somente uma string, porem eu a dividi em duas linhas para torna-la mais legivel
+    //esta, na realidade é somente uma string, porém eu a dividi em duas linhas para torna-la mais legivel
     var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=dogs&format=json&jsoncallback=?";
 
     $.getJSON(url, function(flickrResponse){
-        //iremos simpesmente exibir a resposta no console por enquanto
+        //iremos simplesmente exibir a resposta no console por enquanto
         console.log(flickrResponse);
     });
 };
