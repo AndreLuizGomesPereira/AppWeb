a3af975d8fa5f68e1110720f6b31531fdd6b196a
André Luiz <andregomesenator@gmail.com>
2020-03-16 10:30:45 -0300

    Publicação dos arquivos dos exercicios do livro.

---
+++ chapter5/Flickr/js/app.js
@@ -0,0 +1,28 @@
+var main = function (){
+
+    //esta, na realidade é somente uma string, porém eu a dividi em duas linhas para torna-la mais legivel
+    var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=dogs&format=json&jsoncallback=?";
+
+<<<<<<< HEAD
+    $.getJSON(url, function(flickrResponse){
+        flickrResponse.items.forEach(function(photo){
+            console.log(photo.media.m);
+        });
+    });
+=======
+   $.getJSON(url, function(flickrResponse){
+       flickrResponse.items.forEach(function(photo){
+           //cria um novo elemento jQuery para armazenar a imagem, mas oculta-o para que possamos fazer o fadeIn
+           var $img = $("<img>").hide();
+
+           //define o atributo usando url contido na resposta
+           $img.attr("src", photo.media.m);
+
+           //associa a tag img ao elemento main photos e faz o seu fadeIn
+           $("main .photos").append($img);
+           $img.fadeIn();
+       });
+   });
+>>>>>>> amazeriffic
+};
+$(document).ready(main);
